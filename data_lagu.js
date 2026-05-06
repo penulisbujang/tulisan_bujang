673e8311989e5bb07397fbc3aad516016c6b43e7
penulisbujang <23410473@students.uii.ac.id>
2026-05-06 11:31:34 +0700

    Add files via upload

---
+++ data_lagu.js
@@ -0,0 +1,9 @@
+const dataLagu = [
+  { id: "lagu-1", judul: "Yung Kai - Blue", file: "lagu-1.m4a" },
+  { id: "lagu-2", judul: "Yung Kai - Wildflower", file: "lagu-2.m4a" },
+  {id: "lagu-3", judul: "Ed Sheeran - Perfect", file: "lagu-3.m4a"},
+  {id: "lagu-4", judul: "Westlife - Beautiful in white", file: "lagu-4.m4a"},
+  {id: "lagu-5", judul: "When I Fly Towards You", file: "lagu-5.m4a"},
+  {id: "lagu-6", judul: "Joji - Glimpse of Us", file: "lagu-6.m4a"},
+  {id: "lagu-7", judul: "Ed Sheeran - Photograph", file: "lagu-7.m4a"},
+];
