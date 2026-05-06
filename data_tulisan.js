8c4b987c5fc2f9600568534d65d3e5d329c33c3f
penulisbujang <23410473@students.uii.ac.id>
2026-05-06 07:51:41 +0700

    Add files via upload

---
+++ data_tulisan.js
@@ -0,0 +1,278 @@
+const dataTulisan = [
+  {
+    "Judul": "Kritik Historis UUD 1945 Perspektif Konstitusionalisme",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Teori Konstitusi & Ketatanegaraan",
+    "Link": "https://pinterhukum.or.id/kritik-historis-uud-1945-perspektif-konstitusionalisme/"
+  },
+  {
+    "Judul": "Konsep Hierarki Norma Hukum dalam Stufenbau Theorie Kelsen",
+    "Kategori": "Teori & Filsafat Hukum",
+    "Sub Kategori": "Hierarki Norma Hukum",
+    "Link": "https://pinterhukum.or.id/konsep-hierarki-norma-hukum-dalam-stufenbau-theorie-kelsen/"
+  },
+  {
+    "Judul": "Apakah Perlu Pembentukan Kementerian Dibatasi APBN?",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Sistem Pemerintahan & Pemilu",
+    "Link": "https://pinterhukum.or.id/apakah-perlu-pembentukan-kementerian-dibatasi-apbn/"
+  },
+  {
+    "Judul": "Rekonstruksi Teori Konstitusi Perspektif Filsafat Ilmu",
+    "Kategori": "Teori & Filsafat Hukum",
+    "Sub Kategori": "Filsafat Hukum & Etika",
+    "Link": "https://pinterhukum.or.id/rekonstruksi-teori-konstitusi-perspektif-filsafat-ilmu/"
+  },
+  {
+    "Judul": "Hubungan Industrial Pancasila",
+    "Kategori": "Hukum Ketenagakerjaan & Agraria",
+    "Sub Kategori": "Hukum Ketenagakerjaan",
+    "Link": "https://pinterhukum.com/hubungan-industrial-pancasila/"
+  },
+  {
+    "Judul": "Konsep Hierarki Norma Hukum Hans Nawiasky",
+    "Kategori": "Teori & Filsafat Hukum",
+    "Sub Kategori": "Hierarki Norma Hukum",
+    "Link": "https://pinterhukum.or.id/konsep-hierarki-norma-hukum-hans-nawiasky/"
+  },
+  {
+    "Judul": "Kesadaran Beretika dalam Bingkai Konstitusi",
+    "Kategori": "Teori & Filsafat Hukum",
+    "Sub Kategori": "Filsafat Hukum & Etika",
+    "Link": "https://pinterhukum.or.id/kesadaran-beretika-dalam-bingkai-konstitusi/"
+  },
+  {
+    "Judul": "Perjanjian Kerja Sebagai Special Agreement Antara Hukum Perdata dan Hukum Ketenagakerjaan",
+    "Kategori": "Hukum Ketenagakerjaan & Agraria",
+    "Sub Kategori": "Hukum Ketenagakerjaan",
+    "Link": "https://pinterhukum.or.id/perjanjian-kerja-sebagai-special-agreement-antara-hukum-perdata-dan-hukum-ketenagakerjaan/"
+  },
+  {
+    "Judul": "Perlukah Model Pengujian Konstitusional Asing Diadopsi Mahkamah Konstitusi?",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Lembaga Peradilan & Judicial Review",
+    "Link": "https://pinterhukum.or.id/perlukah-model-pengujian-konstitusional-asing-diadopsi-mahkamah-konstitusi/"
+  },
+  {
+    "Judul": "Komparasi Normatif Pengembangan Kompetensi PNS dan PPPK",
+    "Kategori": "Hukum Ketenagakerjaan & Agraria",
+    "Sub Kategori": "Kepegawaian Negara",
+    "Link": "https://pinterhukum.or.id/komparasi-normatif-pengembangan-kompetensi-pns-dan-pppk/"
+  },
+  {
+    "Judul": "Komparasi Legitimasi Kekuasaan Negara Perspektif Etika Politik dan Positivistik",
+    "Kategori": "Teori & Filsafat Hukum",
+    "Sub Kategori": "Filsafat Hukum & Etika",
+    "Link": "https://pinterhukum.or.id/komparasi-legitimasi-kekuasaan-negara-perspektif-etika-politik-dan-positivistik/"
+  },
+  {
+    "Judul": "Kritik Pandangan Pembelaan Pancasila di Era Orde Baru",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Teori Konstitusi & Ketatanegaraan",
+    "Link": "https://smartlawyer.id/kritik-pandangan-pembelaan-pancasila-di-era-orde-baru/"
+  },
+  {
+    "Judul": "Teori Lembaga Peradilan Konstitusi dalam Konteksnya di Indonesia",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Lembaga Peradilan & Judicial Review",
+    "Link": "https://smartlawyer.id/teori-lembaga-peradilan-konstitusi-dalam-konteksnya-di-indonesia/"
+  },
+  {
+    "Judul": "Stufenbau Theorie Hans Kelsen dalam Kacamata Hukum Indonesia",
+    "Kategori": "Teori & Filsafat Hukum",
+    "Sub Kategori": "Hierarki Norma Hukum",
+    "Link": "https://smartlawyer.id/stufenbau-theorie-hans-kelsen/"
+  },
+  {
+    "Judul": "Rehabilitasi Undang-Undang Komisi Yudisial, Open Legal Policy pada Putusan No. 005/PUU-IV/2006",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Lembaga Peradilan & Judicial Review",
+    "Link": "https://smartlawyer.id/rehabilitasi-undang-undang-komisi-yudisial/"
+  },
+  {
+    "Judul": "Menelaah Piagam Madinah sebagai Konstitusi Modern",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Sejarah & Peradaban",
+    "Link": "https://duniasantri.co/menelaah-piagam-madinah-sebagai-konstitusi-modern/"
+  },
+  {
+    "Judul": "Islamic Worldview versus Filsafat Ilmu",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Filsafat & Sosial Keagamaan",
+    "Link": "https://duniasantri.co/islamic-worldview-versus-filsafat-ilmu/"
+  },
+  {
+    "Judul": "Sekilas Pergeseran Paradigma Peradaban Islam",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Sejarah & Peradaban",
+    "Link": "https://duniasantri.co/sekilas-pergeseran-paradigma-peradaban-islam/"
+  },
+  {
+    "Judul": "Selayang Pandang Dinamika Historis Hukum Islam",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Sejarah & Peradaban",
+    "Link": "https://duniasantri.co/selayang-pandang-dinamika-historis-hukum-islam/"
+  },
+  {
+    "Judul": "Bahasa, Sejarah, dan Pemikiran dalam Kajian Islam",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Sejarah & Peradaban",
+    "Link": "https://duniasantri.co/bahasa-sejarah-dan-pemikiran-dalam-kajian-islam/"
+  },
+  {
+    "Judul": "Peran Cendikiawan Muslim dalam Masyarakat Majemuk",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Filsafat & Sosial Keagamaan",
+    "Link": "https://duniasantri.co/peran-cendekiawan-muslim-dalam-masyarakat-majemuk/"
+  },
+  {
+    "Judul": "Menyorot Program Reforma Agraria di Desa Batulawang",
+    "Kategori": "Hukum Ketenagakerjaan & Agraria",
+    "Sub Kategori": "Hukum Agraria",
+    "Link": "https://literasihukum.com/menyorot-program-reforma-agraria-di-desa-batulawang"
+  },
+  {
+    "Judul": "Rekonstruksi Konstitusi, Antara Fakta Historis dan Cita-cita",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Teori Konstitusi & Ketatanegaraan",
+    "Link": "https://literasihukum.com/rekonstruksi-konstitusi-antara-fakta-historis-dan-cita-cita"
+  },
+  {
+    "Judul": "Telaah Normatif Bentrokan Hak Asasi Manusia dengan Netralitas ASN dalam Kebebasan Hak Berbicara",
+    "Kategori": "Hukum Ketenagakerjaan & Agraria",
+    "Sub Kategori": "Kepegawaian Negara",
+    "Link": "https://literasihukum.com/bentrokan-ham-dengan-netralitas-asn"
+  },
+  {
+    "Judul": "Penggalian Nilai Konstitusional Freedom from Religion dan Komparasi Konsepsi Hak Asasi Manusia",
+    "Kategori": "Hak Asasi Manusia (HAM)",
+    "Sub Kategori": "Konstitusi & HAM",
+    "Link": "https://literasihukum.com/penggalian-nilai-konstitusional-freedom-from-religion-dan-komparasi-konsepsi-hak-asasi-manusia"
+  },
+  {
+    "Judul": "Telaah Posisi Mahkamah Konstitusi dalam Sengketa Pemilihan Kepala Daerah",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Lembaga Peradilan & Judicial Review",
+    "Link": "https://literasihukum.com/posisi-mk-dalam-sengketa-pilkada"
+  },
+  {
+    "Judul": "Historisitas Mahkamah Konstitusi, Gagasan Independensi Lembaga Peradilan",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Lembaga Peradilan & Judicial Review",
+    "Link": "https://literasihukum.com/historisitas-mahkamah-konstitusi"
+  },
+  {
+    "Judul": "Telaah \"Kegentingan Memaksa\" Dalam UU Cipta Kerja Sebagai Unsur Pemberlakuan Hukum Darurat Negara",
+    "Kategori": "Hukum Ketenagakerjaan & Agraria",
+    "Sub Kategori": "Hukum Ketenagakerjaan",
+    "Link": "https://literasihukum.com/telaah-kegentingan-memaksa-dalam-uu-cipta-kerja"
+  },
+  {
+    "Judul": "Prospek Ekstensifikasi Wewenang Mahkamah Konstitusi Dalam Perkara Perselisihan Hasil Pemilihan Umum",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Lembaga Peradilan & Judicial Review",
+    "Link": "https://literasihukum.com/ekstensifikasi-wewenang-mahkamah-konstitusi"
+  },
+  {
+    "Judul": "Instalasi Hak Asasi Manusia dalam Pandangan Konstitusionalisme",
+    "Kategori": "Hak Asasi Manusia (HAM)",
+    "Sub Kategori": "Konstitusi & HAM",
+    "Link": "https://literasihukum.com/hak-asasi-manusia-pandangan-konstitusionalisme"
+  },
+  {
+    "Judul": "Analisis Kritis Purifikasi Hukum Hans Kelsen dari Perspektif Sosiologis dan Etis",
+    "Kategori": "Teori & Filsafat Hukum",
+    "Sub Kategori": "Filsafat Hukum & Etika",
+    "Link": "https://literasihukum.com/purifikasi-hukum-hans-kelsen"
+  },
+  {
+    "Judul": "Bagaimana Konstitusi Memandang Pendidikan?",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Hak Konstitusional & Pendidikan",
+    "Link": "https://medium.com/@penulis_bujang/bagaimana-konstitusi-memandang-pendidikan-1e35ac9569cc"
+  },
+  {
+    "Judul": "Membaca Ulang Pengujian Norma Hukum lewat Hierarki Fungsional",
+    "Kategori": "Teori & Filsafat Hukum",
+    "Sub Kategori": "Hierarki Norma Hukum",
+    "Link": "https://medium.com/@penulis_bujang/membaca-ulang-pengujian-norma-hukum-lewat-hierarki-fungsional-352c44fa8839"
+  },
+  {
+    "Judul": "Apakah Bijak Menjauhi Teks Demi Nalar Yang Liar?",
+    "Kategori": "Opini & Pemikiran Bebas",
+    "Sub Kategori": "Refleksi Personal",
+    "Link": "https://medium.com/@penulis_bujang/apakah-bijak-menjauhi-teks-demi-nalar-yang-liar-f3a81f7e7401"
+  },
+  {
+    "Judul": "Apakah Sebuah Keharusan Meninggalkan Teks Demi Menjadi Modern?",
+    "Kategori": "Opini & Pemikiran Bebas",
+    "Sub Kategori": "Refleksi Personal",
+    "Link": "https://medium.com/@penulis_bujang/apakah-sebuah-keharusan-meninggalkan-teks-demi-menjadi-modern-1aacbfe732ea"
+  },
+  {
+    "Judul": "Negara Kita Masih Negara Hukum, Kan?",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Teori Konstitusi & Ketatanegaraan",
+    "Link": "https://medium.com/@penulis_bujang/berita-terbaru-menunjukkan-bagaimana-hiruk-pikuknya-sistem-ketatanegaraan-indonesia-669fb9a54522"
+  },
+  {
+    "Judul": "Pesan yang Perlu Disampaikan",
+    "Kategori": "Opini & Pemikiran Bebas",
+    "Sub Kategori": "Refleksi Personal",
+    "Link": "https://medium.com/@penulis_bujang/pesan-yang-perlu-disampaikan-c05a18be7cd6"
+  },
+  {
+    "Judul": "Kritik Tindak Pidana terhadap Ideologi Negara",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Ideologi & Ketatanegaraan",
+    "Link": "https://medium.com/@penulis_bujang/kritik-tindak-pidana-terhadap-ideologi-negara-56b73ab6230a"
+  },
+  {
+    "Judul": "Kekacauan Sistemik Demokratisasi Negara",
+    "Kategori": "Opini & Pemikiran Bebas",
+    "Sub Kategori": "Sosial Politik",
+    "Link": "https://medium.com/@penulis_bujang/kekacauan-sistemik-demokratisasi-negara-a498d1df211a"
+  },
+  {
+    "Judul": "Keresahanku Tentang Islam",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Filsafat & Sosial Keagamaan",
+    "Link": "https://medium.com/@penulis_bujang/keresahanku-tentang-islam-6ae1d47a7b6d"
+  },
+  {
+    "Judul": "Apakah HAM Memang Harmonis dengan Islam?",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Islam & Sistem Modern",
+    "Link": "https://medium.com/@penulis_bujang/apakah-ham-memang-harmonis-dengan-islam-27c7efbe82fc"
+  },
+  {
+    "Judul": "Islam, Sekularisme, dan Demokrasi Liberal",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Islam & Sistem Modern",
+    "Link": "https://medium.com/@penulis_bujang/islam-sekularisme-dan-demokrasi-liberal-cc797564aca1"
+  },
+  {
+    "Judul": "Sikap Islam terhadap Tradisi/Hukum Pra Islam",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Sejarah & Peradaban",
+    "Link": "https://medium.com/@penulis_bujang/sikap-islam-terhadap-tradisi-hukum-pra-islam-1318d39fbf64"
+  },
+  {
+    "Judul": "Memang Benar Agama Memiskinkan Umatnya?",
+    "Kategori": "Kajian Islam & Agama",
+    "Sub Kategori": "Filsafat & Sosial Keagamaan",
+    "Link": "https://medium.com/@penulis_bujang/memang-benar-agama-memiskinkan-umatnya-1f1ac50b00b2"
+  },
+  {
+    "Judul": "Ternyata Move On Sesusah Itu",
+    "Kategori": "Opini & Pemikiran Bebas",
+    "Sub Kategori": "Refleksi Personal",
+    "Link": "https://medium.com/@penulis_bujang/ternyata-move-on-sesusah-itu-f3b8ddebd108"
+  },
+  {
+    "Judul": "Dinamika Pengaturan Ketua dan Wakil Ketua MK",
+    "Kategori": "Hukum Tata Negara & Konstitusi",
+    "Sub Kategori": "Lembaga Peradilan & Judicial Review",
+    "Link": "https://medium.com/@penulis_bujang/dinamika-pengaturan-ketua-dan-wakil-ketua-mk-881f92bb54a0"
+  }
+];
